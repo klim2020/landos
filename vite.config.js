4b04fdb367cd8143e933f1572be95b92638f07f0
klim <klimworkss@gmail.com>
2024-01-29 11:19:59 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default {
         { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
     ]
   },
-  build: { chunkSizeWarningLimit: 1600, }
+  build: { chunkSizeWarningLimit: 1600, },
+  base: '/landos/'
 
 }
